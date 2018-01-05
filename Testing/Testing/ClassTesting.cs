2994972152fab81ddc835daab916214309a34d78
Newjan Thevarajah <newj0001@student.eadania.dk>
2018-01-05 10:33:52 +0100

    hej222

--- Testing/Testing/ClassTesting.cs
+++ Testing/Testing/ClassTesting.cs
@@ -8,8 +8,8 @@ namespace Testing
 {
     class ClassTesting
     {
-        
-            string hej = "hej med dig";
+        public int number { get; set; }
+        string hej = "hej med dig";
 
             int Num = 0;
 
